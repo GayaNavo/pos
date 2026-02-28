0ef8d9aa703e271d9a995e8bdc75ead94ae9804b
JAYA00Wick <nipunajayawickrama2000@gmail.com>
2026-02-28 21:17:34 +0530

    removed unused variables

--- RestaurantsDemoBackend-main/controllers/currencyController/currencyController.js
+++ RestaurantsDemoBackend-main/controllers/currencyController/currencyController.js
@@ -25,11 +25,7 @@ const createCurrency = async (req, res) => {
             currencyCode,
             currencySymbole,
         });
-        const testCurrency = new Currency({
-            currencyName,
-            currencyCode,
-            currencySymbole,
-        });
+        
         await newCurrency.save();
         res.status(201).json({ message: 'Currency created successfully', data: newCurrency });
     } catch (error) {
