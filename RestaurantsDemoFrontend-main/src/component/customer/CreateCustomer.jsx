a378a1415a9007a30728dfc061e01b718c0f0e1e
Kdisss <kalana545908@gmail.com>
2026-03-21 20:55:05 +0530

    ok

--- RestaurantsDemoFrontend-main/src/component/customer/CreateCustomer.jsx
+++ RestaurantsDemoFrontend-main/src/component/customer/CreateCustomer.jsx
@@ -89,7 +89,7 @@ function CreateCustomerBody() {
         }
     };
 
-    // Handle clear operation
+    // Handle clear operations
     const handleClear = () => {
         setName('');
         setMobile('');
